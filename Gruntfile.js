56d4ec4d1ef3fd2f950e50167741ede282c05262
Harry Mumford-Turner <harrymt@me.com>
2015-08-02 23:54:38 +0100

    Moved from CodeKit to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,33 +5,59 @@ module.exports = function (grunt) {
   });
 
   grunt.initConfig({
+
     sass: {
       dist: {
         options: {
           style: 'compressed'
         },
         files: {
-          'css/style.css': 'css/style.scss'
+          'css/style.css': 'scss/style.scss'
         }
       }
     },
 
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/dev/jquery.min.js', 'js/dev/googleA.js', 'js/dev/player.js'],
+        dest: 'js/main.min.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'js/main.min.js': ['js/main.min.js']
+        }
+      }
+    },
 
     watch: {
       css: {
-        files: ['css/*.scss'],
+        files: ['scss/*.scss'],
         tasks: ['sass'],
         options: {
           spawn: false
         }
+      },
+
+      javascript: {
+        files: ['js/dev/*.js'],
+        tasks: ['concat', 'uglify']
       }
     },
   });
 
+  grunt.loadNpmTasks('grunt-contrib-concat'); // Concatenate JS
+  grunt.loadNpmTasks('grunt-contrib-uglify'); // Minify JS
   grunt.loadNpmTasks('grunt-contrib-sass'); // Process Sass files
-
   grunt.loadNpmTasks('grunt-contrib-watch'); // On file update, do task
-
   grunt.loadNpmTasks('grunt-serve'); // Local server
 
   grunt.registerTask('default', []);
