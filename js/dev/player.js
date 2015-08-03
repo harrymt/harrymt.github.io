7d37516c97c960cf1f62cdb68ade70a99c995e87
Harry Mumford-Turner <harrymt@me.com>
2015-08-03 13:39:51 +0100

    Image optimised, html improvements, scss refactoring, added mussoc
    Added Illustrator template to project.
    Optimised all images.
    Removed unused icon CSS styles.
    Tidied up CSS heading comments.
    Tidied up spotify ID naming.
    Added Mussoc to projects.
    Rearranged projects

--- js/dev/player.js
+++ js/dev/player.js
@@ -1,22 +1,5 @@
 
 //
-// Scrolling portfolio pieces
-//
-var $pieces = $(".pieces");
-var page = 0;
-if ($pieces.length > 1) {
-    $("#work").append('<a href="#" id="showmore">Show more</a>');
-    $(document).on("click", "#showmore", function(e) {
-        e.preventDefault();
-        $pieces.eq(++page).show();
-        if (page === $pieces.length - 1) {
-            $("#showmore").remove()
-        }
-    })
-}
-
-
-//
 // Spotify now playing
 //
 $.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=harrymf&api_key=7b853a347df8cfe29314dcd7b73444cb&format=json', function(data) {
@@ -25,11 +8,11 @@ $.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ha
         var nowplaying = data.recenttracks.track[0];
         var artistAndSong = nowplaying.name + " - " + nowplaying.artist["#text"];
         var url = data.recenttracks.track[0].url;
-        $('#js-nowplaying').text(artistAndSong);
-        $('#spotify').find('#js-nowplaying-url').attr("href", url);
-		    $('#spotify').css("display", "block");
+        $('.js-nowplaying').text(artistAndSong);
+        $('.js-spotify').find('.js-nowplaying-url').attr("href", url);
+		    $('.js-spotify').css("display", "block");
     } else {
-        $('#spotify').hide();
+        $('.js-spotify').hide();
     }
 });
 
