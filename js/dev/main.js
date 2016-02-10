0d28c2990b8cf0855fe52061b5e37cfdb59d473f
Harry Mumford-Turner <harrymt@me.com>
2016-02-10 01:23:56 +0000

    Stopped ProcessingJS running on mobile

--- js/dev/main.js
+++ js/dev/main.js
@@ -1,3 +1,19 @@
 //
 // main.js
 //
+
+function IsMobile() {
+ return !!( navigator.userAgent.match(/Android/i) ||
+     navigator.userAgent.match(/webOS/i) ||
+     navigator.userAgent.match(/iPhone/i) ||
+     navigator.userAgent.match(/iPad/i) ||
+     navigator.userAgent.match(/iPod/i) ||
+     navigator.userAgent.match(/BlackBerry/i) ||
+     navigator.userAgent.match(/Windows Phone/i)
+ );
+}
+
+// Stop canvas from being drawn on mobiles
+if(IsMobile() || $(window).width() < 500) {
+  $('#processing-background').css("display", "none");
+}
