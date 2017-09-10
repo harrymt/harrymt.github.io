39024c1a75d009c98c4c6507437f91d2e1c2e4b0
Harry Mumford-Turner <harrymt@me.com>
2017-09-10 09:57:23 +0100

    Removed service worker

--- js/dev/main.js
+++ js/dev/main.js
@@ -1,16 +1,4 @@
 
 (function() {
-
-  if('serviceWorker' in navigator) {
-    console.log("Service worker is in navigator");
-    navigator.serviceWorker.register('/sw.js', { scope: '/' })
-      .then(function(registration) {
-            console.log('Service Worker Registered');
-    }).catch(function(error) {
-      console.log('Registration failed with ' + error);
-    });
-    navigator.serviceWorker.ready.then(function(registration) {
-       console.log('Service Worker Ready');
-    });
-  }
+  // Unused
 })();
