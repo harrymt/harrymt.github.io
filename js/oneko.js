d18a4d23bca16a2779c567124ccbdf28b399d0ca
Harry Mumford-Turner <harrymt@users.noreply.github.com>
2025-03-07 10:09:05 +0000

    @@

--- js/oneko.js
+++ js/oneko.js
@@ -96,7 +96,7 @@
     nekoEl.style.top = `${nekoPosY - 16}px`;
     nekoEl.style.zIndex = 2147483647;
 
-    let nekoFile = './img/oneko.gif';
+    let nekoFile = '/images/oneko.gif';
     const curScript = document.currentScript;
     if (curScript && curScript.dataset.cat) {
       nekoFile = curScript.dataset.cat;
